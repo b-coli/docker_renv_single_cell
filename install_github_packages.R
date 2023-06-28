34570390a384081518bbbbc86fbc7c265b5c5dbc
Benjamin Jeremy Cole <BJCole@lbl.gov>
2023-06-28 08:12:37 -0700

    Modifications to v3.0; added DEGreport, SCDC, and xbioc packages

--- install_github_packages.R
+++ install_github_packages.R
@@ -4,3 +4,6 @@ remotes::install_github("velocyto-team/velocyto.R", ref = "83e6ed92c2d9c9640122d
 remotes::install_github("BUStools/BUSpaRse", ref = "5b23c9b609ea20259110eb2592720a6019751a90", clean = TRUE)
 remotes::install_github("cole-trapnell-lab/leidenbase", upgrade = "never", ref = "v0.1.9", clean = TRUE)
 remotes::install_github('cole-trapnell-lab/monocle3', upgrade = "never", ref = "1.0.0", clean = TRUE)
+
+remotes::install_github("renozao/xbioc", upgrade = "never", ref = "1354168bd7e64be4ee1f9f74e971a61556d75003", clean = TRUE)
+remotes::install_github("meichendong/SCDC", upgrade = "never", ref = "890c604eebd7fffa4a08d7344fbd516df6efcf8d", clean = TRUE)
